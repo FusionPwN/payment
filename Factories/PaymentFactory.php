00532715602404fe2f56534ad9b1fb73c52af952
FusionPwN <fusionpwnu@gmail.com>
2023-08-23 11:18:36 +0100

    Update PaymentFactory.php

--- Factories/PaymentFactory.php
+++ Factories/PaymentFactory.php
@@ -12,32 +12,34 @@ declare(strict_types=1);
  *
  */
 
-namespace Vanilo\Payment\Factories;
+namespace App\Factories\Payment;
 
 use Vanilo\Contracts\Payable;
 use Vanilo\Payment\Contracts\Payment;
 use Vanilo\Payment\Contracts\PaymentMethod;
 use Vanilo\Payment\Events\PaymentCreated;
 use Vanilo\Payment\Models\PaymentProxy;
+use Vanilo\Payment\Models\PaymentStatusProxy;
 
 class PaymentFactory
 {
-    public static function createFromPayable(
-        Payable $payable,
-        PaymentMethod $paymentMethod,
-        array $extraData = []
-    ): Payment {
-        $payment = PaymentProxy::create([
-            'amount' => $payable->getAmount(),
-            'currency' => $payable->getCurrency(),
-            'payable_type' => $payable->getPayableType(),
-            'payable_id' => $payable->getPayableId(),
-            'payment_method_id' => $paymentMethod->id,
-            'data' => $extraData
-        ]);
+	public static function createFromPayable(
+		Payable $payable,
+		PaymentMethod $paymentMethod,
+		array $extraData = []
+	): Payment {
+		$payment = PaymentProxy::create([
+			'amount' => $paymentMethod->isCardPayment() ? $payable->card_used_balance : $payable->getAmount(),
+			'currency' => $payable->getCurrency(),
+			'payable_type' => $payable->getPayableType(),
+			'payable_id' => $payable->getPayableId(),
+			'payment_method_id' => $paymentMethod->id,
+			'data' => $extraData,
+			'status' => $paymentMethod->isCardPayment() ? PaymentStatusProxy::PAID() : PaymentStatusProxy::PENDING(),
+		]);
 
-        event(new PaymentCreated($payment));
+		event(new PaymentCreated($payment));
 
-        return $payment;
-    }
+		return $payment;
+	}
 }
